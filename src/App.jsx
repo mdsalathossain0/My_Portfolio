84fb271609f29c246a6bf17e886b2efbc501f378
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-06-24 13:04:07 +0600

    Create Components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from './components/Container'
+import Flex from './components/Flex'
+
+const App = () => {
+  return (
+    <section >
+     <Container>
+      <Flex>
+        <p>pisygjpj</p>
+        <p>pisygjpj</p>
+      </Flex>
+     </Container>
+    </section>
+  )
+}
+
+export default App
