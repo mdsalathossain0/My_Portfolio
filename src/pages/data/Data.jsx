40db5dc88db024721d37331d81176e6c8315f32b
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-07-01 16:31:10 +0600

    projectdata create

---
+++ src/pages/data/Data.jsx
@@ -0,0 +1,129 @@
+
+import Html1 from '../../assets/foodi.PNG'
+import Html2 from '../../assets/nexcent.PNG'
+import Html3 from '../../assets/omah.PNG'
+import Html4 from '../../assets/fresh.PNG'
+import Html5 from '../../assets/shoescart.PNG'
+import Html6 from '../../assets/animation.PNG'
+import Html7 from '../../assets/startup.PNG'
+
+import Java1 from '../../assets/crud.PNG'
+import Java2 from '../../assets/mathsol.PNG'
+import Java3 from '../../assets/guessnum.PNG'
+import Java4 from '../../assets/digitalclock.PNG'
+import Java5 from '../../assets/counterjs.PNG'
+import Java6 from '../../assets/fclick.PNG'
+import Java7 from '../../assets/domclick.PNG'
+
+import React1 from '../../assets/ecommerce.PNG'
+import React2 from '../../assets/inspiration.PNG'
+import React3 from '../../assets/innovate.PNG'
+
+
+let projectData =[
+    {
+        id:1,
+        image:Html1,
+        title:'Foodi',
+        category:'html'
+    },
+    {
+        id:2,
+        image:Html2,
+        title:'Nextcent',
+        category:'html'
+    },
+    {
+        id:3,
+        image:Html3,
+        title:'Omah',
+        category:'html'
+    },
+    {
+        id:4,
+        image:Html4,
+        title:'Fresh',
+        category:'html'
+    },
+    {
+        id:5,
+        image:Html5,
+        title:'Shoes Cart',
+        category:'html'
+    },
+    {
+        id:6,
+        image:Html6,
+        title:'Animation',
+        category:'html'
+    },
+    {
+        id:7,
+        image:Html7,
+        title:'StartUp',
+        category:'html'
+    },
+    {
+        id:8,
+        image:Java1,
+        title:'CRUD Operation',
+        category:'javascript'
+    },
+    {
+        id:9,
+        image:Java2,
+        title:'Math Solutions',
+        category:'javascript'
+    },
+    {
+        id:10,
+        image:Java3,
+        title:'Number Guess Game',
+        category:'javascript'
+    },
+    {
+        id:11,
+        image:Java4,
+        title:'Digital Clock',
+        category:'javascript'
+    },
+    {
+        id:12,
+        image:Java5,
+        title:'Counter Js',
+        category:'javascript'
+    },
+    {
+        id:13,
+        image:Java6,
+        title:'Funny Click',
+        category:'javascript'
+    },
+    {
+        id:14,
+        image:Java7,
+        title:'Dom Click',
+        category:'javascript'
+    },
+    {
+        id:15,
+        image:React1,
+        title:'Orebi eCommerce',
+        category:'react'
+    },
+    {
+        id:16,
+        image:React2,
+        title:'Inspiration',
+        category:'react'
+    },
+    {
+        id:17,
+        image:React3,
+        title:'Innovate',
+        category:'react'
+    },
+]
+
+
+export default projectData
