45fe1629c7a9aaa7f1ae18f28a6145e86fced809
mdsalathossain0 <mdsalathossain0@gmail.com>
2025-07-08 19:19:16 +0600

    Next arrow fixed

--- src/components/SampleNextArrow.jsx
+++ src/components/SampleNextArrow.jsx
@@ -4,7 +4,7 @@ import { IoIosArrowForward } from 'react-icons/io'
 const SampleNextArrow = ({ className, style, onClick }) => {
   return (
     <div className={`${className} relative`} onClick={onClick}> 
-     <div className='w-[55px] h-[40px] bg-third z-10 flex justify-center items-center rounded-lg absolute -bottom-[118px] left-2/3 lg:left-1/2 -translate-y-1/2 -translate-x-1/2'> <IoIosArrowForward className='text-white text-xl' /></div>
+     <div className='w-[55px] h-[40px] bg-third z-10 flex justify-center items-center rounded-lg absolute -bottom-[97px] left-2/3 lg:left-1/2 -translate-x-1/2'> <IoIosArrowForward className='text-white text-xl' /></div>
     </div>
   )
 }
